e402c450d016aa8d5161e5d9dc7916c867a148d3
ricardosantosdeassis <ricardosantosdeassis@yahoo.com.br>
2025-07-29 17:34:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,27 @@
-const CACHE_NAME = "treino-cache-v1";
+
+const CACHE_NAME = "treino-cache-v2";
 const urlsToCache = [
   "./",
   "./index.html",
-  "./dieta.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => k !== CACHE_NAME && caches.delete(k))
+    ))
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
